da4b245e2394970a2ff55df0dbb325eb50a3cd8b
teachmetw <srobinlee.teachmetw@gmail.com>
2023-09-09 23:01:13 -0700

    removed env

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,14 @@
 import Input from "@/components/input";
+import axios from "axios";
 import { useCallback, useState } from "react"
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
 const Auth = () => {
+    const router = useRouter();
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -11,6 +18,35 @@ const Auth = () => {
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, [])
+
+    const login = useCallback(async () => {
+        try {
+          await signIn('credentials', {
+            email,
+            password,
+            redirect: false,
+            callbackUrl: '/'
+          });
+    
+          router.push('/');
+        } catch (error) {
+          console.log(error);
+        }
+      }, [email, password, router]);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/auth/register', {
+                email,
+                name,
+                password
+            });
+            login();
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, name, password, login])  
+
 
 
     return (
@@ -48,9 +84,39 @@ const Auth = () => {
                                 onChange={(ev) => setPassword(ev.target.value)}
                             />
                         </div>
-                        <button className="bg-purple-600 py-3 text-white rounded-md w-full mt-10 hover:bg-purple-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-purple-600 py-3 text-white rounded-md w-full mt-10 hover:bg-purple-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign Up'}
                         </button>
+                        
+                        <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div className="
+                                    w-10
+                                    h-10
+                                    bg-white
+                                    rounded-full
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:opacity-80
+                                    transition">
+                                    <FcGoogle size={30} />
+                                </div>
+                                <div className="
+                                    w-10
+                                    h-10
+                                    bg-white
+                                    rounded-full
+                                    flex
+                                    items-center
+                                    justify-center
+                                    cursor-pointer
+                                    hover:opacity-80
+                                    transition">
+                                    <FaGithub size={30} />
+                                </div>
+                        </div>
+
                         <p className="text-neutral-500 mt-12">
                             {variant === 'login' ? 'Don\'t have an account?' : 'Already have an account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
